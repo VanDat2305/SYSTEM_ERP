58a39d33f510710a6c4a3916a618fb0228d9b034
datmv <datmv202@gmail.com>
2025-06-27 17:32:09 +0700

    Revert "demo account service"
    This reverts commit 3fca28ff7b8ba38e3e6097ffe0a7b87d3891ac5b.

--- Modules/Account/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('account::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('account.name') !!}</p>
-@endsection
--- resources/views/emails/temp_account.blade.php
+++
@@ -1,7 +0,0 @@
-<p>Xin chào {{ $name }},</p>
-<p>Bạn vừa được tạo tài khoản trên hệ thống dịch vụ. Đây là thông tin đăng nhập tạm thời:</p>
-<ul>
-    <li>Email: <strong>{{ $email }}</strong></li>
-    <li>Mật khẩu tạm thời: <strong>{{ $password }}</strong></li>
-</ul>
-<p>Vui lòng đăng nhập và đổi mật khẩu ngay sau khi sử dụng lần đầu.</p>
