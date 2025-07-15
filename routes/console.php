bba6685e8c54cd444528c07aa3a55ce839ddd7a4
datmv <datmv202@gmail.com>
2025-07-15 10:07:00 +0700

    fix lỗi command

--- routes/console.php
+++ routes/console.php
@@ -1,17 +1,23 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
+// use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Schedule;
+// Đăng ký custom command (nếu cần)
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-// Đây là cách đăng ký command tự định nghĩa
-// Mọi schedule phải nằm trong closure return phía dưới
+Schedule::command('orders:auto-complete')->everyMinute();
+Schedule::command('notify:package-expiry')->dailyAt('08:00');
 
-return function (Schedule $schedule) {
-    $schedule->command('notify:package-expiry')->dailyAt('08:00');
-    $schedule->command('orders:auto-complete')->everyFifteenMinutes();
-};
+// Đăng ký schedule, tất cả lệnh nằm trong closure return phía dưới
+// return function (Schedule $schedule) {
+//     $schedule->command('notify:package-expiry')->dailyAt('08:00');
+//     // $schedule->command('orders:auto-complete')->everyMinute();
+//     $schedule->command('orders:auto-complete')->everyMinute()->withoutOverlapping();
+//     $schedule->command('inspire')
+//             ->everyMinute()
+//             ->appendOutputTo(storage_path('logs/schedule.log'));
+// };
